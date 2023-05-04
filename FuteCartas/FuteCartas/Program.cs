ea9e137be46ec5cbc1beacafdf2a58d06a9e0f12
FernandoGutemberg <fernando70sil@gmail.com>
2023-05-04 20:39:07 -0300

    Primeiro Commit
    Dado inicio ao projeto.

---
+++ FuteCartas/FuteCartas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuteCartas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int EncerrarJogo = 0;
+            Random sorte = new Random();
+            while (EncerrarJogo == 0)
+            {
+                bool sair = false;
+                int r = 0;
+                bool rodada = false;
+                PlayerX p = new PlayerX();
+                PlayerX p2 = new PlayerX();
+                p.Reset();
+                p2.Reset();
+                Console.Clear();
+
+                rodada = sorte.Next(1, 3) == 1 ? true : false;
+
+
+                while (sair == false)
+                {
+
+                    if (rodada == false)
+                    {
+                        Console.ReadKey();
+                        Console.Clear();
+                        r++;
+                        rodada = true;
+                    }
+
+                    if (rodada == true)
+                    {
+                        Console.ReadKey();
+                        Console.Clear();
+                        r++;
+                        rodada = false;
+                    }
+
+                    if (p2.Acabou == true && p.Acabou == true) { sair = true; }
+
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("Deseja Continuar? \n [ 0 ] - Sim; \n [ 1 ] - Não;");
+                EncerrarJogo = int.Parse(Console.ReadLine());
+
+            }
+        }
+    }
+}
---
+++ FuteCartas/FuteCartas/Textos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuteCartas
+{
+    public class Textos
+    {
+        public const string LBordadaUp = "╔══════════════════════════════════════════════════╗";
+        public const string LBordadaDown = "╚══════════════════════════════════════════════════╝";
+        public string[] textoJogo = new string[]
+        {
+           /* 0 */ "--- Bem vindo ao jogo de cartas e futebol ---",
+           /* 1 */ "GOOOOOOOOOOOLLLL!!!! Do ",
+           /* 2 */ "Total de gols é de ",
+           /* 3 */ "Incrivel defesa do Goleiro!! Não foi dessa fez que vemos um Gol!",
+           /* 4 */ "Parabens! ",
+           /* 5 */ "É o(a) ganhador(a)!",
+           /* 6 */ "Infelizmente o jogador ",
+           /* 7 */ "não ganhou dessa vez!",
+           /* 8 */ "Apesar do empate, parabens aos jogadores! ",
+           /* 9 */ "Penalidade MAXIMA!",
+           /* 10 */ "É uma boa chance de tentar marcar um gol",
+           /* 11 */ "Rodada da vez : Jogador ",
+           /* 12 */ "Pontuação: ",
+           /* 13 */ "║                Jogador  {0,-23}  ║",
+           /* 14 */ "Energia restante: ",
+           /* 15 */ "║ Pontuação: {0,-37} ║",
+           /* 16 */ "║ Gols marcados: {0,-34}║",
+           /* 17 */ "║ Energia restante: {0,-30} ║"
+
+        };
+
+    }
+}
